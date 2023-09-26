96e675f6dadca56fb20e7ba50f8c05c1de087bf9
NetherVikings <Jeremiahhay@hotmail.com>
2023-09-26 11:58:00 -0600

    Create temp-conversion.py

---
+++ temp-conversion.py
@@ -0,0 +1,13 @@
+
+
+choice = input("Do you want Celcius or Fareinheit? (C/F?)")
+
+if choice == "F" :
+   fareinheit = input("What is the tempurature in Celcius?")
+   answer = int(fareinheit) * 1.8 + 32
+elif choice == "C":
+   celcius = input("What is the tempurature in Fareinheit?")
+   answer = (int(celcius) - 32.0) * .555555
+print ('%.1f' %answer)
+
+
